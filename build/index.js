ec83a7d0c89083241930f93f8686b53f1c802838
gaaveloso <gabrielvelosorezende-po@hotmail.com>
2023-01-11 15:40:02 -0300

    fluxo de dados backend

--- build/index.js
+++ build/index.js
@@ -12,113 +12,304 @@ app.use((0, cors_1.default)());
 app.listen(3003, () => {
     console.log(" 🔥 Servidor rodando na porta 3003");
 });
-app.get('/ping', (req, res) => {
-    res.send('Pong!');
+app.get("/ping", (req, res) => {
+    res.send("Pong!");
 });
-app.get('/users', (req, res) => {
-    res.status(200).send(database_1.users);
+app.get("/users", (req, res) => {
+    try {
+        res.status(200).send(database_1.users);
+    }
+    catch (error) {
+        console.log(error);
+        if (res.statusCode === 200) {
+            res.status(500);
+        }
+        res.send(error.message);
+    }
 });
-app.get('/products', (req, res) => {
-    res.status(200).send(database_1.products);
+app.get("/products", (req, res) => {
+    try {
+        res.status(200).send(database_1.products);
+    }
+    catch (error) {
+        console.log(error);
+        if (res.statusCode === 200) {
+            res.status(500);
+        }
+        res.send(error.message);
+    }
 });
-app.get('/purchases', (req, res) => {
+app.get("/purchases", (req, res) => {
     res.status(200).send(database_1.purchases);
 });
-app.get('/product/search', (req, res) => {
-    const q = req.query.q;
-    res.status(200).send((0, database_1.getProductsByName)(q));
+app.get("/product/search", (req, res) => {
+    let productFilter;
+    try {
+        const q = req.query.q;
+        if (q.length <= 1) {
+            res.status(400);
+            throw new Error("query params deve possuir pelo menos um caractere");
+        }
+        productFilter = database_1.products.filter((product) => {
+            return product.name.toLowerCase().includes(q.toLowerCase());
+        });
+        res.status(200).send(productFilter);
+    }
+    catch (error) {
+        console.log(error);
+        if (res.statusCode === 200) {
+            res.status(500);
+        }
+        res.send(error.message);
+    }
 });
-app.post('/users', (req, res) => {
-    const id = req.body.id;
-    const email = req.body.email;
-    const password = req.body.password;
-    const newUser = {
-        id,
-        email,
-        password
-    };
-    database_1.users.push(newUser);
-    res.status(201).send('Usuario criado com sucesso 😎');
+app.post("/users", (req, res) => {
+    try {
+        const id = req.body.id;
+        const email = req.body.email;
+        const password = req.body.password;
+        const findId = database_1.users.find((user) => user.id === id);
+        if (findId) {
+            res.status(400);
+            throw new Error("ID indisponivel");
+        }
+        const findEmail = database_1.users.find((user) => user.email === email);
+        if (findEmail) {
+            res.status(400);
+            throw new Error("EMAIL indisponivel");
+        }
+        const newUser = {
+            id,
+            email,
+            password,
+        };
+        database_1.users.push(newUser);
+        res.status(201).send("Usuario criado com sucesso 😎");
+    }
+    catch (error) {
+        console.log(error);
+        if (res.statusCode === 200) {
+            res.status(500);
+        }
+        res.send(error.message);
+    }
 });
-app.post('/products', (req, res) => {
-    const id = req.body.id;
-    const name = req.body.name;
-    const price = req.body.price;
-    const category = req.body.category;
-    const newProduct = {
-        id,
-        name,
-        price,
-        category,
-    };
-    database_1.products.push(newProduct);
-    res.status(201).send('Produto criado com sucesso 😎');
+app.post("/products", (req, res) => {
+    try {
+        const id = req.body.id;
+        const name = req.body.name;
+        const price = req.body.price;
+        const category = req.body.category;
+        const findId = database_1.products.find((product) => product.id === id);
+        if (findId) {
+            res.status(400);
+            throw new Error("ID indisponivel");
+        }
+        const newProduct = {
+            id,
+            name,
+            price,
+            category,
+        };
+        database_1.products.push(newProduct);
+        res.status(201).send("Produto criado com sucesso 😎");
+    }
+    catch (error) {
+        console.log(error);
+        if (res.statusCode === 200) {
+            res.status(500);
+        }
+        res.send(error.message);
+    }
 });
-app.post('/purchases', (req, res) => {
-    const userId = req.body.userId;
-    const productId = req.body.productId;
-    const quantity = req.body.quantity;
-    const totalPrice = req.body.totalPrice;
-    const newPurchase = {
-        userId,
-        productId,
-        quantity,
-        totalPrice,
-    };
-    database_1.purchases.push(newPurchase);
-    res.status(201).send('Compra efetuada com sucesso 😎');
+app.post("/purchases", (req, res) => {
+    try {
+        const userId = req.body.userId;
+        const productId = req.body.productId;
+        const quantity = req.body.quantity;
+        const totalPrice = req.body.totalPrice;
+        const findIdUser = database_1.purchases.find((purchase) => purchase.userId === userId);
+        if (!findIdUser) {
+            res.status(400);
+            throw new Error("ID do usuario não existe");
+        }
+        const findIdProduct = database_1.purchases.find((purchase) => purchase.productId === productId);
+        if (!findIdProduct) {
+            res.status(400);
+            throw new Error("ID do produto não existe");
+        }
+        const newPurchase = {
+            userId,
+            productId,
+            quantity,
+            totalPrice,
+        };
+        database_1.purchases.push(newPurchase);
+        res.status(201).send("Compra efetuada com sucesso 😎");
+    }
+    catch (error) {
+        console.log(error);
+        if (res.statusCode === 200) {
+            res.status(500);
+        }
+        res.send(error.message);
+    }
 });
-app.get('/products/:id', (req, res) => {
-    const id = req.params.id;
-    const result = database_1.products.find((product) => product.id === id);
-    res.status(200).send(result);
-    console.log('Objeto Product Encontrado');
+app.get("/products/:id", (req, res) => {
+    try {
+        const id = req.params.id;
+        const findProduct = database_1.products.find((product) => product.id === id);
+        if (!findProduct) {
+            res.status(400);
+            throw new Error("Produto não encontrado");
+        }
+        const result = database_1.products.find((product) => product.id === id);
+        res.status(200).send(result);
+        console.log("Objeto Product Encontrado");
+    }
+    catch (error) {
+        console.log(error);
+        if (res.statusCode === 200) {
+            res.status(500);
+        }
+        res.send(error.message);
+    }
 });
-app.get('/users/:id/purchases', (req, res) => {
-    const id = req.params.id;
-    const result = database_1.purchases.find((purchase) => purchase.userId === id);
-    res.status(200).send(result);
-    console.log('Array de compras do user procurado');
+app.get("/users/:id/purchases", (req, res) => {
+    try {
+        const id = req.params.id;
+        const findUser = database_1.purchases.find((purchase) => purchase.userId === id);
+        if (!findUser) {
+            res.status(400);
+            throw new Error("Usuario não encontrado");
+        }
+        const result = database_1.purchases.find((purchase) => purchase.userId === id);
+        res.status(200).send(result);
+        console.log("Array de compras do user procurado");
+    }
+    catch (error) {
+        console.log(error);
+        if (res.statusCode === 200) {
+            res.status(500);
+        }
+        res.send(error.message);
+    }
 });
 app.delete("/user/:id", (req, res) => {
-    const id = req.params.id;
-    const indexToRemove = database_1.users.findIndex((user) => user.id === id);
-    if (indexToRemove >= 0) {
-        database_1.users.splice(indexToRemove, 1);
+    try {
+        const id = req.params.id;
+        const findUser = database_1.users.find((user) => user.id === id);
+        if (!findUser) {
+            res.status(400);
+            throw new Error("Usuario não encontrado");
+        }
+        const indexToRemove = database_1.users.findIndex((user) => user.id === id);
+        if (indexToRemove >= 0) {
+            database_1.users.splice(indexToRemove, 1);
+        }
+        res.status(200).send("User apagado com sucesso");
     }
-    res.status(200).send("User apagado com sucesso");
+    catch (error) {
+        console.log(error);
+        if (res.statusCode === 200) {
+            res.status(500);
+        }
+        res.send(error.message);
+    }
 });
 app.delete("/product/:id", (req, res) => {
-    const id = req.params.id;
-    const indexToRemove = database_1.products.findIndex((product) => product.id === id);
-    if (indexToRemove >= 0) {
-        database_1.products.splice(indexToRemove, 1);
+    try {
+        const id = req.params.id;
+        const findProduct = database_1.products.find((product) => product.id === id);
+        if (!findProduct) {
+            res.status(400);
+            throw new Error("Produto não encontrado");
+        }
+        const indexToRemove = database_1.products.findIndex((product) => product.id === id);
+        if (indexToRemove >= 0) {
+            database_1.products.splice(indexToRemove, 1);
+        }
+        res.status(200).send("Produto apagado com sucesso");
     }
-    res.status(200).send("Produto apagado com sucesso");
+    catch (error) {
+        console.log(error);
+        if (res.statusCode === 200) {
+            res.status(500);
+        }
+        res.send(error.message);
+    }
 });
-app.put('/user/:id', (req, res) => {
-    const id = req.params.id;
-    const newEmail = req.body.email;
-    const newPassword = req.body.password;
-    const user = database_1.users.find((user) => user.id === id);
-    if (user) {
-        user.email = newEmail || user.email;
-        user.password = newPassword || user.password;
+app.put("/user/:id", (req, res) => {
+    try {
+        const id = req.params.id;
+        const findUser = database_1.users.find((user) => user.id === id);
+        if (!findUser) {
+            res.status(400);
+            throw new Error("Usuario não encontrado");
+        }
+        const newEmail = req.body.email;
+        const newPassword = req.body.password;
+        if (newEmail !== undefined) {
+            if (!newEmail.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/)) {
+                res.status(400);
+                throw new Error("Email invalido");
+            }
+        }
+        if (newPassword !== undefined) {
+            if (!newPassword.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{4,12}$/g)) {
+                res.status(400);
+                throw new Error("'password' deve possuir entre 4 e 12 caracteres, com letras maiúsculas e minúsculas e no mínimo um número e um caractere especial");
+            }
+        }
+        const user = database_1.users.find((user) => user.id === id);
+        if (user) {
+            user.email = newEmail || user.email;
+            user.password = newPassword || user.password;
+        }
+        res.status(200).send("Cadastro atualizado com sucesso");
     }
-    res.status(200).send("Cadastro atualizado com sucesso");
+    catch (error) {
+        console.log(error);
+        if (res.statusCode === 200) {
+            res.status(500);
+        }
+        res.send(error.message);
+    }
 });
-app.put('/product/:id', (req, res) => {
-    const id = req.params.id;
-    const newName = req.body.name;
-    const newPrice = req.body.price;
-    const newCategory = req.body.category;
-    const product = database_1.products.find((product) => product.id === id);
-    if (product) {
-        product.name = newName || product.name;
-        product.price = newPrice || product.price;
-        product.category = newCategory || product.category;
-        product.price = isNaN(newPrice) ? product.price : newPrice;
+app.put("/product/:id", (req, res) => {
+    try {
+        const id = req.params.id;
+        const findProduct = database_1.products.find((product) => product.id === id);
+        if (!findProduct) {
+            res.status(400);
+            throw new Error("Produto não encontrado");
+        }
+        const newName = req.body.name;
+        const newPrice = req.body.price;
+        const newCategory = req.body.category;
+        if (newName.length < 1) {
+            res.status(400);
+            throw new Error("'newName' deve ter pelo menos 1 caractere");
+        }
+        if (typeof newPrice !== "number") {
+            res.status(400);
+            throw new Error("'newPrice' deve ser um 'number'");
+        }
+        const product = database_1.products.find((product) => product.id === id);
+        if (product) {
+            product.name = newName || product.name;
+            product.price = newPrice || product.price;
+            product.category = newCategory || product.category;
+            product.price = isNaN(newPrice) ? product.price : newPrice;
+        }
+        res.status(200).send("Produto atualizado com sucesso");
     }
-    res.status(200).send("Produto atualizado com sucesso");
+    catch (error) {
+        console.log(error);
+        if (res.statusCode === 200) {
+            res.status(500);
+        }
+        res.send(error.message);
+    }
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,167 +1,423 @@
-import { users, products, purchases, getAllUsers, getAllProducts, getProductsByName, createUser, getAllPurchases } from "./database";
-import express, { Request, Response } from 'express'
-import cors from 'cors'
+import {
+  users,
+  products,
+  purchases,
+  getAllUsers,
+  getAllProducts,
+  getProductsByName,
+  createUser,
+  getAllPurchases,
+} from "./database";
+import express, { Request, Response } from "express";
+import cors from "cors";
 import { TProduct, TPurchase, TUser, Department } from "./types";
 
 // console.table(users)
 // console.table(products)
 // console.table(purchases)
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 app.listen(3003, () => {
-    console.log(" 🔥 Servidor rodando na porta 3003");
-})
+  console.log(" 🔥 Servidor rodando na porta 3003");
+});
 
-app.get('/ping', (req: Request, res: Response) => {
-    res.send('Pong!')
-})
+app.get("/ping", (req: Request, res: Response) => {
+  res.send("Pong!");
+});
 
-app.get('/users', (req: Request, res: Response) => {
-    res.status(200).send(users)
-})
+app.get("/users", (req: Request, res: Response) => {
+  try {
+    res.status(200).send(users);
+  } catch (error: any) {
+    console.log(error);
 
-app.get('/products', (req: Request, res: Response) => {
-    res.status(200).send(products)
-})
+    if (res.statusCode === 200) {
+      res.status(500);
+    }
 
-app.get('/purchases', (req: Request, res: Response) => {
-    res.status(200).send(purchases)
-})
+    res.send(error.message);
+  }
+});
 
-app.get('/product/search', (req: Request, res: Response) => {
-    const q = req.query.q as string
+app.get("/products", (req: Request, res: Response) => {
+  try {
+    res.status(200).send(products);
+  } catch (error: any) {
+    console.log(error);
 
-    res.status(200).send(getProductsByName(q))
-})
+    if (res.statusCode === 200) {
+      res.status(500);
+    }
 
-app.post('/users', (req: Request, res: Response) => {
-    const id = req.body.id
-    const email = req.body.email
-    const password = req.body.password
+    res.send(error.message);
+  }
+});
+
+app.get("/purchases", (req: Request, res: Response) => {
+  res.status(200).send(purchases);
+});
+
+app.get("/product/search", (req: Request, res: Response) => {
+  let productFilter;
+  try {
+    const q = req.query.q as string;
+
+    if (q.length <= 1) {
+      res.status(400);
+      throw new Error("query params deve possuir pelo menos um caractere");
+    }
+
+    productFilter = products.filter((product) => {
+      return product.name.toLowerCase().includes(q.toLowerCase());
+    });
+    res.status(200).send(productFilter);
+  } catch (error: any) {
+    console.log(error);
+
+    if (res.statusCode === 200) {
+      res.status(500);
+    }
+
+    res.send(error.message);
+  }
+});
+
+app.post("/users", (req: Request, res: Response) => {
+  try {
+    const id = req.body.id;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const findId = users.find((user) => user.id === id);
+
+    if (findId) {
+      res.status(400);
+      throw new Error("ID indisponivel");
+    }
+
+    const findEmail = users.find((user) => user.email === email);
+
+    if (findEmail) {
+      res.status(400);
+      throw new Error("EMAIL indisponivel");
+    }
 
     const newUser: TUser = {
-        id,
-        email,
-        password
+      id,
+      email,
+      password,
+    };
+
+    users.push(newUser);
+    res.status(201).send("Usuario criado com sucesso 😎");
+  } catch (error: any) {
+    console.log(error);
+
+    if (res.statusCode === 200) {
+      res.status(500);
     }
 
-    users.push(newUser)
-    res.status(201).send('Usuario criado com sucesso 😎')
-    // res.status(201).send(createUser(id, email, password))
-})
+    res.send(error.message);
+  }
+});
 
-app.post('/products', (req: Request, res: Response) => {
-    const id = req.body.id
-    const name = req.body.name
-    const price = req.body.price
-    const category = req.body.category
+app.post("/products", (req: Request, res: Response) => {
+  try {
+    const id = req.body.id;
+    const name = req.body.name;
+    const price = req.body.price;
+    const category = req.body.category;
+
+    const findId = products.find((product) => product.id === id);
+
+    if (findId) {
+      res.status(400);
+      throw new Error("ID indisponivel");
+    }
 
     const newProduct: TProduct = {
-        id,
-        name,
-        price,
-        category,
+      id,
+      name,
+      price,
+      category,
+    };
+
+    products.push(newProduct);
+    res.status(201).send("Produto criado com sucesso 😎");
+  } catch (error: any) {
+    console.log(error);
+
+    if (res.statusCode === 200) {
+      res.status(500);
     }
 
-    products.push(newProduct)
-    res.status(201).send('Produto criado com sucesso 😎')
-})
+    res.send(error.message);
+  }
+});
 
-app.post('/purchases', (req: Request, res: Response) => {
-    const userId = req.body.userId
-    const productId = req.body.productId
-    const quantity = req.body.quantity
-    const totalPrice = req.body.totalPrice
+app.post("/purchases", (req: Request, res: Response) => {
+  try {
+    const userId = req.body.userId;
+    const productId = req.body.productId;
+    const quantity = req.body.quantity;
+    const totalPrice = req.body.totalPrice;
+
+    const findIdUser = purchases.find((purchase) => purchase.userId === userId);
+
+    if (!findIdUser) {
+      res.status(400);
+      throw new Error("ID do usuario não existe");
+    }
+
+    const findIdProduct = products.find(
+      (product) => product.id === productId
+    );
+
+    if (!findIdProduct) {
+      res.status(400);
+      throw new Error("ID do produto não existe");
+    }
+
+    
+    if (findIdProduct.price * quantity !== totalPrice) {
+        res.status(400)
+        throw new Error("Total incorreto");
+    }
 
     const newPurchase: TPurchase = {
-        userId,
-        productId,
-        quantity,
-        totalPrice,
+      userId,
+      productId,
+      quantity,
+      totalPrice,
+    };
+
+    purchases.push(newPurchase);
+    res.status(201).send("Compra efetuada com sucesso 😎");
+  } catch (error: any) {
+    console.log(error);
+
+    if (res.statusCode === 200) {
+      res.status(500);
     }
 
-    purchases.push(newPurchase)
-    res.status(201).send('Compra efetuada com sucesso 😎')
-})
+    res.send(error.message);
+  }
+});
 
-app.get('/products/:id', (req: Request, res: Response) => {
-    const id = req.params.id
+app.get("/products/:id", (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
 
-    const result = products.find((product) => product.id === id)
+    const findProduct = products.find((product) => product.id === id);
+    if (!findProduct) {
+      res.status(400);
+      throw new Error("Produto não encontrado");
+    }
 
-    res.status(200).send(result)
-    console.log('Objeto Product Encontrado')
-})
+    const result = products.find((product) => product.id === id);
 
-app.get('/users/:id/purchases', (req: Request, res: Response) => {
-    const id = req.params.id
+    res.status(200).send(result);
+    console.log("Objeto Product Encontrado");
+  } catch (error: any) {
+    console.log(error);
 
-    const result = purchases.find((purchase) => purchase.userId === id)
+    if (res.statusCode === 200) {
+      res.status(500);
+    }
 
-    res.status(200).send(result)
-    console.log('Array de compras do user procurado')
-})
+    res.send(error.message);
+  }
+});
+
+app.get("/users/:id/purchases", (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+
+    const findUser = purchases.find((purchase) => purchase.userId === id);
+    if (!findUser) {
+      res.status(400);
+      throw new Error("Usuario não encontrado");
+    }
+
+    const result = purchases.find((purchase) => purchase.userId === id);
+
+    res.status(200).send(result);
+    console.log("Array de compras do user procurado");
+  } catch (error: any) {
+    console.log(error);
+
+    if (res.statusCode === 200) {
+      res.status(500);
+    }
+
+    res.send(error.message);
+  }
+});
 
 app.delete("/user/:id", (req: Request, res: Response) => {
-    const id = req.params.id
+  try {
+    const id = req.params.id;
 
-    const indexToRemove = users.findIndex((user) => user.id === id)
-
-    if(indexToRemove >= 0){
-        users.splice(indexToRemove, 1)
+    const findUser = users.find((user) => user.id === id);
+    if (!findUser) {
+      res.status(400);
+      throw new Error("Usuario não encontrado");
     }
 
-    res.status(200).send("User apagado com sucesso")
-})
+    const indexToRemove = users.findIndex((user) => user.id === id);
+
+    if (indexToRemove >= 0) {
+      users.splice(indexToRemove, 1);
+    }
+
+    res.status(200).send("User apagado com sucesso");
+  } catch (error: any) {
+    console.log(error);
+
+    if (res.statusCode === 200) {
+      res.status(500);
+    }
+
+    res.send(error.message);
+  }
+});
 
 app.delete("/product/:id", (req: Request, res: Response) => {
-    const id = req.params.id
+  try {
+    const id = req.params.id;
 
-    const indexToRemove = products.findIndex((product) => product.id === id)
-
-    if(indexToRemove >= 0){
-        products.splice(indexToRemove, 1)
+    const findProduct = products.find((product) => product.id === id);
+    if (!findProduct) {
+      res.status(400);
+      throw new Error("Produto não encontrado");
     }
 
-    res.status(200).send("Produto apagado com sucesso")
-})
+    const indexToRemove = products.findIndex((product) => product.id === id);
 
-app.put('/user/:id', (req: Request, res: Response) => {
-    const id = req.params.id
-
-    const newEmail = req.body.email as string | undefined
-    const newPassword = req.body.password as string | undefined
-
-    const user = users.find((user) => user.id === id)
-
-    if(user) {
-        user.email = newEmail || user.email
-        user.password = newPassword || user.password
+    if (indexToRemove >= 0) {
+      products.splice(indexToRemove, 1);
     }
 
-    res.status(200).send("Cadastro atualizado com sucesso")
-})
+    res.status(200).send("Produto apagado com sucesso");
+  } catch (error: any) {
+    console.log(error);
 
-app.put('/product/:id', (req: Request, res: Response) => {
-    const id = req.params.id
-
-    const newName = req.body.name as string | undefined
-    const newPrice = req.body.price as number | undefined
-    const newCategory = req.body.category as Department | undefined
-
-    const product = products.find((product) => product.id === id)
-
-    if(product) {
-        product.name = newName || product.name
-        product.price = newPrice || product.price
-        product.category = newCategory || product.category
-
-        product.price = isNaN(newPrice) ? product.price : newPrice
+    if (res.statusCode === 200) {
+      res.status(500);
     }
 
-    res.status(200).send("Produto atualizado com sucesso")
-})
+    res.send(error.message);
+  }
+});
+
+app.put("/user/:id", (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+
+    const findUser = users.find((user) => user.id === id);
+    if (!findUser) {
+      res.status(400);
+      throw new Error("Usuario não encontrado");
+    }
+
+    const newEmail = req.body.email as string | undefined;
+    const newPassword = req.body.password as string | undefined;
+
+    if (newEmail === req.body.email) {
+      res.status(400);
+      throw new Error("Email igual ao anterior");
+    }
+
+    const regexEmail =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+    const regexPassword =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{4,12}$/g;
+    
+    if (newEmail !== undefined) {
+      if (!newEmail.match(regexEmail)) {
+        res.status(400);
+        throw new Error("Email invalido");
+      }
+    }
+
+    if (newPassword !== undefined) {
+      if (typeof newPassword !== "string") {
+        res.status(400);
+        throw new Error("'password' deve ser uma string");
+      }
+      if (!newPassword.match(regexPassword)) {
+        res.status(400);
+        throw new Error(
+          "'password' deve possuir entre 4 e 12 caracteres, com letras maiúsculas e minúsculas e no mínimo um número e um caractere especial"
+        );
+      }
+    }
+    const user = users.find((user) => user.id === id);
+
+    if (user) {
+      user.email = newEmail || user.email;
+      user.password = newPassword || user.password;
+    }
+
+    res.status(200).send("Cadastro atualizado com sucesso");
+  } catch (error: any) {
+    console.log(error);
+
+    if (res.statusCode === 200) {
+      res.status(500);
+    }
+
+    res.send(error.message);
+  }
+});
+
+app.put("/product/:id", (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+
+    const findProduct = products.find((product) => product.id === id);
+    if (!findProduct) {
+      res.status(400);
+      throw new Error("Produto não encontrado");
+    }
+
+    const newName = req.body.name as string | undefined;
+    const newPrice = req.body.price as number | undefined;
+    const newCategory = req.body.category as Department | undefined;
+
+    if (newName.length < 1) {
+      res.status(400);
+      throw new Error("'newName' deve ter pelo menos 1 caractere");
+    }
+
+    if (typeof newPrice !== "number") {
+      res.status(400);
+      throw new Error("'newPrice' deve ser um 'number'");
+    }
+
+    const product = products.find((product) => product.id === id);
+
+    if (product) {
+      product.name = newName || product.name;
+      product.price = newPrice || product.price;
+      product.category = newCategory || product.category;
+
+      product.price = isNaN(newPrice) ? product.price : newPrice;
+    }
+
+    res.status(200).send("Produto atualizado com sucesso");
+  } catch (error: any) {
+    console.log(error);
+
+    if (res.statusCode === 200) {
+      res.status(500);
+    }
+
+    res.send(error.message);
+  }
+});
