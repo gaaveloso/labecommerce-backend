370ff91c6f3c3211a807e2dace1a99cce9acecf5
gaaveloso <gabrielvelosorezende-po@hotmail.com>
2023-01-18 15:24:47 -0300

    relacoes-sql-i

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -9,12 +9,22 @@ CREATE TABLE
 
 INSERT INTO
     users(id, email, password)
-VALUES 
-    ("1","veloso@labenu.com","labenu123"),
-    ("2","velosodev@labenu.com","123labenu"),
-    ("3","velososql@labenu.com","labenu456");
+VALUES (
+        "1",
+        "veloso@labenu.com",
+        "labenu123"
+    ), (
+        "2",
+        "velosodev@labenu.com",
+        "123labenu"
+    ), (
+        "3",
+        "velososql@labenu.com",
+        "labenu456"
+    );
 
 --retorna todos os usuários cadastrados--
+
 SELECT * FROM users;
 
 CREATE TABLE
@@ -27,22 +37,45 @@ CREATE TABLE
 
 INSERT INTO
     products(id, name, price, category)
-VALUES 
-("10","Memória RAM DDR4",150,"Hardware"),
-("20","Headset com fio USB",120,"Periférico"),
-("30","Placa mãe",500,"Hardware"),
-("40","SSD M.2",500,"Hardware"),
-("50","WebCam",200,"Periférico");
+VALUES (
+        "10",
+        "Memória RAM DDR4",
+        150,
+        "Hardware"
+    ), (
+        "20",
+        "Headset com fio USB",
+        120,
+        "Periférico"
+    ), (
+        "30",
+        "Placa mãe",
+        500,
+        "Hardware"
+    ), (
+        "40",
+        "SSD M.2",
+        500,
+        "Hardware"
+    ), (
+        "50",
+        "WebCam",
+        200,
+        "Periférico"
+    );
 
 --retorna todos os produtos cadastrados--
+
 SELECT * FROM products;
 
 --retorna o produto baseado no termo de busca--
-SELECT * FROM products
-WHERE name LIKE "%placa%";
+
+SELECT * FROM products WHERE name LIKE "%placa%";
 
 --mocke um novo usuário
+
 --insere o item mockado na tabela users
+
 INSERT INTO
     users(id, email, password)
 VALUES (
@@ -52,7 +85,9 @@ VALUES (
     );
 
 --mocke um novo produto
+
 --insere o item mockado na tabela products
+
 INSERT INTO
     products(id, name, price, category)
 VALUES (
@@ -63,41 +98,94 @@ VALUES (
     );
 
 --busca produto baseada no id mockado
-SELECT * FROM products
-WHERE id = "20";
+
+SELECT * FROM products WHERE id = "20";
 
 --busca user baseado no id mockado
-SELECT * FROM users
-WHERE id = "1";
+
+SELECT * FROM users WHERE id = "1";
 
 --delete a linha do produto baseada no id mockado
-DELETE FROM products
-WHERE id = "20";
+
+DELETE FROM products WHERE id = "20";
 
 --edite a linha do user baseada nos valores mockados
-UPDATE users
-SET email = 'veloso2@labenu.com'
-WHERE id = "1";
+
+UPDATE users SET email = 'veloso2@labenu.com' WHERE id = "1";
 
 --edite a linha do product baseada nos valores mockados
-UPDATE products
-SET price = '200.00'
-WHERE id = "20";
+
+UPDATE products SET price = '200.00' WHERE id = "20";
 
 --retorna o resultado ordenado pela coluna name em ordem crescente
+
 --não tenho name então usei email
-SELECT * FROM users
-ORDER BY email ASC;
+
+SELECT * FROM users ORDER BY email ASC;
 
 --retorna o resultado ordenado pela coluna price em ordem crescente
+
 --limite o resultado em 20 iniciando pelo primeiro item
+
 --"Começando do primeiro item, eu pulo o primeiro ?"
+
 --Não tenho 20 produtos mockados
-SELECT * FROM products
-ORDER BY price ASC
-LIMIT 20 OFFSET 1;
+
+SELECT * FROM products ORDER BY price ASC LIMIT 20 OFFSET 1;
 
 --mocke um intervalo de preços, por exemplo entre 100.00 e 300.00
+
 --retorna os produtos com preços dentro do intervalo mockado em ordem crescente
-SELECT * FROM products
-WHERE price >= 100.00 AND price <= 300.00;
+
+SELECT * FROM products WHERE price >= 100.00 AND price <= 300.00;
+
+--Criação da tabela de pedidos
+
+CREATE TABLE
+    purchases(
+        id TEXT PRIMARY KEY UNIQUE NOT NULL,
+        total_price REAL UNIQUE NOT NULL,
+        paid INTEGER NOT NULL,
+        delivered_at TEXT NULL,
+        buyer_id TEXT NOT NULL,
+        FOREIGN Key (buyer_id) REFERENCES users (id)
+    );
+
+UPDATE purchases
+SET delivered_at = datetime()
+WHERE id = "p003";
+
+INSERT INTO
+    purchases (
+        id,
+        total_price,
+        paid,
+        delivered_at,
+        buyer_id
+    )
+VALUES  ("p001", 200.00, 0, 'null', "1"),
+        ("p002", 400.00, 1, 'null', "1"),
+        ("p003", 650.00, 0, 'null', "2"),
+        ("p004", 80.00, 1, 'null', "2");
+
+--Assim não deu certo, porque?
+
+--Caso quisesse colocar datas diferentes pra cada usuario
+
+-- VALUES ("p001", 200.00, 0, SELECT DATE('now'), "1"), ("p002", 400.00, 1, SELECT DATE('now'), "1"), ("p003", 650.00, 0, 'null', "2"), ("p004", 80.00, 1, 'null', "2");
+
+SELECT
+    purchases.id as purchase_id,
+    purchases.total_price,
+    purchases.paid,
+    purchases.delivered_at,
+    purchases.buyer_id as user_id,
+    users.email,
+    users.password
+FROM purchases
+    INNER JOIN users on users.id = purchases.buyer_id;
+
+SELECT *
+FROM purchases
+    INNER JOIN users on users.id = purchases.buyer_id
+WHERE users.id = "2"
